c7aaa52d4506190f08f62c9f7cdc3a9667410b4f
daisylin927 <daisylin927@gmail.com>
2023-06-12 05:28:36 -0700

    first commit

--- app.js
+++ app.js
@@ -1,19 +1,11 @@
-import express from "express";
-import cors from "cors";
+import express from 'express';
+import cors from 'cors';
 import session from "express-session";
-import HelloController from "./controllers/hello-controller.js";
-import UserController from "./controllers/users/users-controller.js";
-import TuitsController from "./controllers/tuits/tuits-controller.js";
-import AuthController from "./controllers/users/auth-controller.js";
-
-const app = express();
-app.use(
-  cors({
-    credentials: true,
-    origin: "http://localhost:3000",
-  })
-);
-app.use(express.json());
+import HelloController from "./controllers/hello-controller.js"
+import UserController from './users/users-controller.js';
+import TuitsController from './controllers/tuits/tuits-controller.js';
+import AuthController from './users/auth-controller.js';
+const app = express()
 app.use(
   session({
     secret: "any string",
@@ -21,10 +13,15 @@ app.use(
     saveUninitialized: true,
   })
 );
-
-HelloController(app);
-UserController(app);
+app.use(express.json());
+app.use(
+  cors({
+    credentials: true,
+    origin: ["http://localhost:3000"],
+  })
+);
 TuitsController(app);
-AuthController(app);
-
-app.listen(process.env.PORT || 4000);
+HelloController(app)
+UserController(app)
+AuthController(app)
+app.listen(process.env.PORT || 4000)
--- users/users-dao.js
+++ users/users-dao.js
@@ -1,6 +1,8 @@
 let users = [];
 
+
 export const findAllUsers = () => users;
+
 
 export const findUserById = (uid) => {
   const index = users.findIndex((u) => u._id === uid);
@@ -8,29 +10,37 @@ export const findUserById = (uid) => {
   return null;
 };
 
+
 export const findUserByUsername = (username) => {
   const index = users.findIndex((u) => u.username === username);
   if (index !== -1) return users[index];
   return null;
 };
+
+
 export const findUserByCredentials = (username, password) => {
-  const index = users.findIndex.find(
-    (u) => u.username === username && u.password === password
-  );
+  const index = users.findIndex((u) => u.username === username && u.password === password);
   if (index !== -1) return users[index];
   return null;
 };
 
+
 export const createUser = (user) => users.push(user);
 
-export const updateUser = (uid, user) => {
-  const index = users.findIndex((u) => u._id === uid);
-  users[index] = { ...users[index], ...user };
-  return { status: "ok" };
+export const updateUser = (uid, body) => {
+  console.log("update body: ", body);
+  console.log("all the users: ", users);
+  console.log("uid - 1", (uid - 1));
+  console.log("real uid?", uid);
+  users = users.map((usr, index) =>
+    index === (uid - 1) ?
+      { ...usr, ...body } : usr
+  );
+  return uid
 };
 
 export const deleteUser = (uid) => {
   const index = users.findIndex((u) => u._id === uid);
   users.splice(index, 1);
-  return { status: "ok" };
+  return { status: 'ok' }
 };
