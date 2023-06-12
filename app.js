98e458342f871ec43adfe533a0f8911e2edc8043
daisylin927 <daisylin927@gmail.com>
2023-06-12 02:27:50 -0700

    first commit

--- app.js
+++ app.js
@@ -1,28 +1,30 @@
-import express from 'express';
-import cors from 'cors'
+import express from "express";
+import cors from "cors";
 import session from "express-session";
 import HelloController from "./controllers/hello-controller.js";
-import UserController from "./users/users-controller.js";
+import UserController from "./controllers/users/users-controller.js";
 import TuitsController from "./controllers/tuits/tuits-controller.js";
-import AuthController from "./users/auth-controller.js";
+import AuthController from "./controllers/users/auth-controller.js";
+
 const app = express();
 app.use(
-    session({
-        secret: "any string",
-        resave: false,
-        saveUninitialized: true,
-    })
-);
-app.use(
-    cors({
-        credentials: true,
-        origin: "http://localhost:3000",
-    })
+  cors({
+    credentials: true,
+    origin: "http://localhost:3000",
+  })
 );
 app.use(express.json());
+app.use(
+  session({
+    secret: "any string",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 HelloController(app);
 UserController(app);
 TuitsController(app);
 AuthController(app);
+
 app.listen(process.env.PORT || 4000);
